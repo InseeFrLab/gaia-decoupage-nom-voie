214bbd755e2c462422ffcdfcabc0868533d07eec
Berova Raya <FI7L7T@ad.insee.intra>
2024-01-31 12:21:41 +0100

    class clean type voie finie

--- src/decoupage_libelles/constants/constant_lists.py
+++ src/decoupage_libelles/constants/constant_lists.py
@@ -1,49 +1,3 @@
-unique_libs_for_codes = ['CALLE', 'DARSE', 'SENTIER', 'VALLEE']
-
-other_libs_for_codes = ['CALLADA', 'DARCE', 'SENTE', 'VALLON']
-
-liste_types_complement_0 = ['IMM', 'IMMEUBLE',
-                            'BLOC',
-                            'BAT', 'BATIMENT']
-
-liste_types_complement_1 = ['PAVILLON', 'PAV',
-                            'LDT',
-                            'IMM', 'IM', 'IMMEUBLE',
-                            'BLOC',
-                            'BAT', 'BATIMENT']
-
-liste_types_compl_immeuble = ['HLM',
-                              'CROIX',
-                              'RUE',
-                              'GALERIE',
-                              'COTE',
-                              'CENTRE',
-                              'HAMEAU',
-                              'DOMAINE',
-                              'MAISON',
-                              'CHALET',
-                              'LOTISSEMENT',
-                              'VILLA',
-                              'PARKING',
-                              'PARC',
-                              'RESIDENCE',
-                              'PLACE',
-                              'QUARTIER',
-                              'ESPACE']
-
-liste_voie_fictive_1 = ['BOULEVARD',
-                        'ALLEE',
-                        'RUE',
-                        'AVENUE',
-                        'IMPASSE',
-                        'CHEMIN',
-                        'VOIE',
-                        'PLACE',
-                        'CHEMINEMENT',
-                        'VOIE COMMUNALE']
-
-liste_voie_fictive_2 = ['ROUTE'] + liste_voie_fictive_1
-
 liste_type_longitudinal = ['ROUTE',
                            'BOULEVARD',
                            'RUE',
@@ -91,15 +45,6 @@ liste_type_agglo = ['DOMAINE',
 
 liste_postag = ['DET', 'ADJ', 'ADP', 'CCONJ']
 
-ponctuations = ['-', '.', ',', ';', ':', '!', '?', '(', ')', '[', ']', '{',
-                '}', "'", '"', '«', '»', '*', '/']
 
 first_type_agglo = ['RESIDENCE']
 
-liste_orthographes_immeuble = ['IM', 'IMM', 'IMMEUBLE']
-
-list_alphabet = ['B', 'C', 'E', 'F', 'G', 'H', 'I', 'J',
-                 'K', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
-                 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-list_num = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-list_fictive = list_alphabet + list_num
