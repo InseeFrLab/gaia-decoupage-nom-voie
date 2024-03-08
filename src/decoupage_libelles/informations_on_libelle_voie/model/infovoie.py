b10f22a2ede85d6f9ad304ab9aa6915336674b2d
Berova Raya <FI7L7T@ad.insee.intra>
2024-03-08 18:01:06 +0100

    correction coquille

--- src/decoupage_libelles/informations_on_libelle_voie/model/infovoie.py
+++ src/decoupage_libelles/informations_on_libelle_voie/model/infovoie.py
@@ -9,7 +9,7 @@ class InfoVoie:
     types_and_positions: Optional[dict] = field(default_factory=dict)
     label_postag: Optional[List[str]] = None
     types_detected: Optional[List[str]] = None
-    has_type_in_first_pos: Optional[bool] = False
-    has_type_in_second_pos: Optional[bool] = False
-    has_type_in_last_pos: Optional[bool] = False
-    has_duplicated_types: Optional[bool] = False
+    has_type_in_first_pos: Optional[bool] = None
+    has_type_in_second_pos: Optional[bool] = None
+    has_type_in_last_pos: Optional[bool] = None
+    has_duplicated_types: Optional[bool] = None
