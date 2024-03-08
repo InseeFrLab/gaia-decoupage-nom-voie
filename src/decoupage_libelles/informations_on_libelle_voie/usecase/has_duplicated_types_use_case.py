639f7fca21fd46be81fe853e1bad071dd09e4fda
Berova Raya <FI7L7T@ad.insee.intra>
2024-03-08 18:00:35 +0100

    correction coquille

--- src/decoupage_libelles/informations_on_libelle_voie/usecase/has_duplicated_types_use_case.py
+++ src/decoupage_libelles/informations_on_libelle_voie/usecase/has_duplicated_types_use_case.py
@@ -3,6 +3,7 @@ from decoupage_libelles.informations_on_libelle_voie.model.infovoie import InfoV
 
 class HasDuplicatedTypesUseCase:
     def execute(self, infovoie: InfoVoie) -> InfoVoie:
+        infovoie.has_duplicated_types = False
         if infovoie.types_and_positions:
             for __, occurence in infovoie.types_and_positions.keys():
                 if occurence > 1:
