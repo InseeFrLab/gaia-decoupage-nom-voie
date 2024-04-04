532e3c8168808f16d315e52592db1c6bd197d237
Julien Gauchet <julien.gauchet@insee.fr>
2024-04-04 15:51:26 +0200

    Correction tests

--- src/decoupage_libelles/entrypoints/web/main_api.py
+++ src/decoupage_libelles/entrypoints/web/main_api.py
@@ -4,31 +4,33 @@ from fastapi.responses import RedirectResponse
 import logging
 from typing import List, Dict
 from decoupage_libelles.config.type_voie_decoupage_launcher import TypeVoieDecoupageLauncher
+from dataclasses import dataclass
 
 
+@dataclass
 class VoiesData(BaseModel):
     list_labels_voies: List[str] = Field(..., example=["rue Hoche", "Residence Soleil Rue des cerisiers"])
 
-    def process(self) -> List[Dict[str, Dict[str, str]]]:
-        self.list_labels_voies = list(set(self.list_labels_voies))
 
-        typevoiedecoupagelauncher: TypeVoieDecoupageLauncher = TypeVoieDecoupageLauncher()
-        voies_processed = typevoiedecoupagelauncher.execute(voies_data=self.list_labels_voies)
+def process(voies_data) -> List[Dict[str, Dict[str, str]]]:
+    list_labels_voies = list(set(voies_data.list_labels_voies))
 
-        voies_processed_dict = [
-            {
-                voie.label_origin.lower() if voie.label_origin else "": {
-                    "numero": voie.num_assigned if voie.num_assigned is not None else "",
-                    "indice_rep": voie.indice_rep.lower() if voie.indice_rep else "",
-                    "typeVoie": voie.type_assigned.lower() if voie.type_assigned else "",
-                    "libelleVoie": voie.label_assigned.lower() if voie.label_assigned else "",
-                    "complementAdresse": voie.compl_assigned.lower() if voie.compl_assigned else "",
-                    "complementAdresse2": voie.compl2.lower() if voie.compl2 else "",
-                }
+    voies_processed = TypeVoieDecoupageLauncher().execute(voies_data=list_labels_voies)
+
+    voies_processed_dict = [
+        {
+            voie.label_origin.lower() if voie.label_origin else "": {
+                "numero": voie.num_assigned if voie.num_assigned is not None else "",
+                "indice_rep": voie.indice_rep.lower() if voie.indice_rep else "",
+                "typeVoie": voie.type_assigned.lower() if voie.type_assigned else "",
+                "libelleVoie": voie.label_assigned.lower() if voie.label_assigned else "",
+                "complementAdresse": voie.compl_assigned.lower() if voie.compl_assigned else "",
+                "complementAdresse2": voie.compl2.lower() if voie.compl2 else "",
             }
-            for voie in voies_processed
-        ]
-        return voies_processed_dict
+        }
+        for voie in voies_processed
+    ]
+    return voies_processed_dict
 
 
 app = FastAPI()
@@ -56,4 +58,4 @@ async def root():
     description="Cette route permet de découper les libellés de voies pour en extraire des types",
 )
 async def analyse_libelles_voies(voies_data: VoiesData):
-    return {"reponse": voies_data.process()}
+    return {"reponse": process(voies_data)}
--- tests/entrypoints/web/test_main_api.py
+++ tests/entrypoints/web/test_main_api.py
@@ -1,6 +1,7 @@
 import logging
 from fastapi.testclient import TestClient
 from decoupage_libelles.entrypoints.web.main_api import app, initialize_api
+from unittest.mock import patch
 
 # Initialisation du logger pour les tests
 logging.basicConfig(level=logging.DEBUG)
@@ -16,11 +17,8 @@ def test_analyse_libelles_voies():
     # Given
     list_labels_voies = {"list_labels_voies": ["rue Hoche", "Residence Soleil Rue des cerisiers"]}
     # When
-    response = client.post("/analyse-libelles-voies", json=list_labels_voies)
-    # Then
-    assert response.status_code == 200
-    assert response.json() == {
-        "reponse": [
+    with patch("decoupage_libelles.entrypoints.web.main_api.process") as mock_process:
+        mock_process.return_value = [
             {"rue hoche": {"numero": "", "indice_rep": "", "typeVoie": "rue", "libelleVoie": "hoche", "complementAdresse": " ", "complementAdresse2": ""}},
             {
                 "residence soleil rue des cerisiers": {
@@ -33,4 +31,22 @@ def test_analyse_libelles_voies():
                 }
             },
         ]
-    }
+        response = client.post("/analyse-libelles-voies", json=list_labels_voies)
+
+        # Then
+        assert response.status_code == 200
+        assert response.json() == {
+            "reponse": [
+                {"rue hoche": {"numero": "", "indice_rep": "", "typeVoie": "rue", "libelleVoie": "hoche", "complementAdresse": " ", "complementAdresse2": ""}},
+                {
+                    "residence soleil rue des cerisiers": {
+                        "numero": "",
+                        "indice_rep": "",
+                        "typeVoie": "rue",
+                        "libelleVoie": "des cerisiers",
+                        "complementAdresse": "residence soleil",
+                        "complementAdresse2": "",
+                    }
+                },
+            ]
+        }
