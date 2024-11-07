e1c81c79c3482cf9fac5af97a55a0689c0760e55
rayaberova <raya.berova@insee.fr>
2024-11-07 16:53:49 +0100

    ajout de regex pour séparer les numeros des lettres au sein du nom de voie + dillatation des synonymes

---
+++ src/decoupage_libelles/prepare_data/ponctuation/usecase/basic_preprocess_words_use_case.py
@@ -0,0 +1,44 @@
+from decoupage_libelles.informations_on_libelle_voie.model.infovoie import InfoVoie
+from unidecode import unidecode
+import re
+
+
+class BasicPreprocessWordsUseCase:
+    EXTRA_SYNONYMS = {
+        'st': 'saint',
+        'ste': 'sainte',
+        'sts': 'saints',
+        'stes': 'saintes',
+        'gd': 'grand',
+        'gde': 'grande',
+        'gds': 'grands',
+        'gdes': 'grandes',
+        'pt': 'petit',
+        'pte': 'petite',
+        'pts': 'petits',
+        'ptes': 'petites',
+        'gen': 'general',
+        'gal': 'general',
+        'mal': 'marechal',
+        'lt': 'lieutenant',
+        'cdt': 'commandant',
+        'dr': 'docteur',
+        'ht': 'haut',
+        'hte': 'haute',
+        'hts': 'hauts',
+        'htes': 'hautes',
+        'pdt': 'president',
+        'sgt': 'sergent',
+        'pr': 'professeur'
+        }
+
+    def execute(self, voie: InfoVoie) -> InfoVoie:
+        voie_ascii_folded = unidecode(voie.label_raw)
+        voie_number_letter_separeted = re.sub(r"(\d+)([a-zA-Z]+)", r"\1 \2", voie_ascii_folded)
+        voie_upper = voie_number_letter_separeted.upper()
+        for acronym, full_form in BasicPreprocessWordsUseCase.EXTRA_SYNONYMS.items():
+            voie_upper = re.sub(rf'\b{acronym}\b', f'{acronym} {full_form}', voie_upper, flags=re.IGNORECASE)        
+
+        voie.label_raw = voie_upper
+
+        return voie
--- src/decoupage_libelles/prepare_data/ponctuation/usecase/separate_complementary_part_use_case.py
+++ src/decoupage_libelles/prepare_data/ponctuation/usecase/separate_complementary_part_use_case.py
@@ -1,27 +1,24 @@
 from decoupage_libelles.informations_on_libelle_voie.model.infovoie import InfoVoie
-from unidecode import unidecode
-
 
 class SeparateComplementaryPartUseCase:
+
     def execute(self, voie: InfoVoie) -> InfoVoie:
-        voie_ascii_folded = unidecode(voie.label_origin)
-        voie_upper = voie_ascii_folded.upper()
-        # split_on_underscore = voie_upper.split(" - ")
-        # split_hashtag = voie_upper.split("#")
+        # split_on_underscore = voie.label_origin.split(" - ")
+        # split_hashtag = voie.label_origin.split("#")
 
         # if len(split_on_underscore) > 1:  # 'R DES NOYERS - SITE-BEAUPLAN'
         #     voie.label_raw = split_on_underscore[0]
         #     voie.complement = split_on_underscore[1]
 
-        # if voie_upper.count("#") == 2:  # 'R DU VAL DE SEVRE #49179#'
+        # if voie.label_origin.count("#") == 2:  # 'R DU VAL DE SEVRE #49179#'
         #     voie.label_raw = split_hashtag[0]
         #     voie.complement = split_hashtag[1]
 
-        # if "(" in voie_upper and ")" in voie_upper and voie_upper.index("(") < voie_upper.index(")"):  # 'R DES NOYERS (SITE-BEAUPLAN)'
-        #     voie.label_raw = voie_upper.split("(")[0].strip()
-        #     voie.complement = voie_upper.split("(")[1].split(")")[0].strip()
+        # if "(" in voie.label_origin and ")" in voie.label_origin and voie.label_origin.index("(") < voie.label_origin.index(")"):  # 'R DES NOYERS (SITE-BEAUPLAN)'
+        #     voie.label_raw = voie.label_origin.split("(")[0].strip()
+        #     voie.complement = voie.label_origin.split("(")[1].split(")")[0].strip()
 
         if not voie.label_raw:
-            voie.label_raw = voie_upper
+            voie.label_raw = voie.label_origin
 
         return voie
