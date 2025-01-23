dd7fd95e6a7b8ef174cad782ea17826b9d42492c
rayaberova <raya.berova@insee.fr>
2025-01-23 12:21:31 +0000

    découpage en parallele sur ls3 ok

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# 
+# Algorithme pour découper type de voie, nom de voie et complément d'adresse à partir d'un libellé de nom de voie brut.
+
+## Getting started
+
+Dans son namespace sur LS, ouvrir un service vs-python en paramétrant les ressources de cette façon : 
+![](data/vers/parametrages_vs_code_decoupage_parallele.png "Paramétrages des ressources du service LS3 vscode")
+
+Lancer dans un terminal bash : 
+```{bash}
+git clone https://gitlab.insee.fr/geographie/gaia/gaia-decoupage-libelles-voies.git
+source ./setup.sh
+```
+Il faudra se munir de son identifiant GitLab et de son token pour pouvoir cloner le projet.
+
+
+## Lancer le traitement d'un fichier
+
+Placer le fichier dans l'espace de stockage s3, et configurer le fichier decoupage_libelles/scripts_parallelises/config.yml :  
+
+- s3_bucket: Dossier où le fichier à traiter se trouve. Ex : "travail/projet-ml-moteur-identification-gaia/confidentiel/personnel_non_sensible".  
+- input_path: Nom du fichier. Ex : "voies_01.csv".  
+- sep: Si c'est un fichier csv, préciser le séparateur. Ex : ",". Si c'est un parquet, mettre "".  
+- encodeur: Si c'est un fichier csv, préciser l'encodeur. Ex : "utf-8". Si c'est un parquet, mettre "".  
+- var_name_nom_voie: Nom de la variable dans laquelle on va extraire le type de voie. Ex : "nom_voie_complet".  
+
+Dans le terminal bash, lancer :  
+```{bash}
+python decoupage_libelles/scripts_parallelises/main.py
+```
+
+Le fichier traité sera enregistré dans le même dossier avec le même format et le même nom de fichier suivi de "_parse".  
--- src/decoupage_libelles/scripts_parallelises/main.py
+++ src/decoupage_libelles/scripts_parallelises/main.py
@@ -1,12 +1,17 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 import pandas as pd
 import s3fs
 from multiprocessing import Pool, cpu_count
-import os
 import time
 from fastapi.testclient import TestClient
 import warnings
 from decoupage_libelles.entrypoints.web.main_api import app, initialize_api
 from tqdm import tqdm
+import yaml
+from yaml.loader import SafeLoader
+import pyarrow.parquet as pq
 
 
 def initialize_client():
@@ -50,9 +55,6 @@ def process_chunk(chunk):
             how="left",
         )
 
-        merged_df["code_insee"] = merged_df["code_insee"].astype(str).str.zfill(5)
-        merged_df["code_postal"] = merged_df["code_postal"].astype(str).str.zfill(5)
-
         merged_df.rename(
             columns={
                 "typeVoie": "type_voie_parse",
@@ -66,7 +68,7 @@ def process_chunk(chunk):
     return merged_df
 
 
-def save_dataframes(dfs):
+def save_dataframes(dfs, file_type):
     """Sauvegarder les DataFrames depuis la queue."""
     fs = s3fs.S3FileSystem(
         client_kwargs={'endpoint_url': 'https://'+'minio.datascience.kube.insee.fr'},
@@ -74,10 +76,18 @@ def save_dataframes(dfs):
         secret=os.environ["AWS_SECRET_ACCESS_KEY"],
         token=os.environ["AWS_SESSION_TOKEN"])
 
-    print(f"Enregistrement des résultats")
+    print("Enregistrement des résultats")
     df_concatenated = pd.concat(dfs, ignore_index=True)
-    with fs.open(output_file, "wb") as f:
-        df_concatenated.to_parquet(f, engine="pyarrow", index=False)
+
+    if file_type == "csv":
+        with fs.open(output_file, "wb") as f:
+            df_concatenated.to_csv(f, index=False)
+
+    elif file_type == "parquet":
+        with fs.open(output_file, "wb") as f:
+            df_concatenated.to_parquet(f, engine="pyarrow", index=False)
+
+    print(f"Le fichier traité a été enregistré ici {output_file}")
 
 
 # Fonction pour lire et traiter un chunk
@@ -91,18 +101,25 @@ def process_and_follow_chunk(args):
 
 
 # Fonction principale pour lire et diviser le fichier en chunks
-def process_file_in_chunks(input_file, chunk_size, sep, encodeur):
+def process_file_in_chunks(input_file, chunk_size, sep, encodeur, file_type):
     """
     Lit le fichier source en morceaux (chunks) et retourne une liste des chunks avec leur ID.
     """
     results = []
     partition_id = 0
-
     with fs.open(input_file, "rb") as f:
-        # Utilisation de tqdm pour afficher une barre de progression
-        for chunk in tqdm(pd.read_csv(f, chunksize=chunk_size, sep=sep, encoding=encodeur, dtype=str), desc="Lecture des chunks"):
-            results.append((chunk, partition_id))
-            partition_id += 1
+        if file_type == "csv":
+            # Utilisation de tqdm pour afficher une barre de progression
+            for chunk in tqdm(pd.read_csv(f, chunksize=chunk_size, sep=sep, encoding=encodeur, dtype=str), desc="Lecture des chunks"):
+                results.append((chunk, partition_id))
+                partition_id += 1
+        elif file_type == "parquet":
+            # Utilisation de tqdm pour afficher une barre de progression
+            parquet_file = pq.ParquetFile(f)
+            for i, batch in tqdm(enumerate(parquet_file.iter_batches(batch_size=chunk_size)), desc="Parsing des voies", unit="chunk"):
+                chunk = batch.to_pandas()
+                results.append((chunk, partition_id))
+                partition_id += 1
 
     return results
 
@@ -118,16 +135,21 @@ if __name__ == "__main__":  # Variables globales
         secret=os.environ["AWS_SECRET_ACCESS_KEY"],
         token=os.environ["AWS_SESSION_TOKEN"])
 
-    s3_bucket = "travail/user-fi7l7t/confidentiel/personnel_non_sensible"
-    input_file = f"s3://{s3_bucket}/adresses_ban_2024-06-01.csv"
-    global output_file
-    output_file = f"s3://{s3_bucket}/adresses_ban_2024-06-01_parse.parquet"
-    chunk_size = 100_000  # Taille des paquets à traiter
-    sep = ";"  # Séparateur CSV
-    encodeur = "utf-8"
-    num_workers = cpu_count()  # Nombre de processus parallèles
+    with open("decoupage_libelles/scripts_parallelises/config.yml") as f:
+        config = yaml.load(f, Loader=SafeLoader)
+
+    s3_bucket = config['s3_bucket']
+    input_path = config['input_path']
+    input_file = f"s3://{s3_bucket}/{input_path}"
+    sep = config["sep"]  # Séparateur CSV
+    encodeur = config["encodeur"]
+    file_type = input_file.split(".")[1]
     global var_name_nom_voie
-    var_name_nom_voie = "nom_voie"
+    var_name_nom_voie = config['var_name_nom_voie']
+    global output_file
+    output_file = input_file.split(".")[0] + "_parse." + input_file.split(".")[1]
+    chunk_size = 1000  # Taille des paquets à traiter
+    num_workers = cpu_count()  # Nombre de processus parallèles
 
     warnings.filterwarnings("ignore", category=FutureWarning)
 
@@ -135,7 +157,7 @@ if __name__ == "__main__":  # Variables globales
     initialize_client()
 
     # Récupérer les chunks à traiter
-    chunks_to_process = process_file_in_chunks(input_file, chunk_size, sep, encodeur)
+    chunks_to_process = process_file_in_chunks(input_file, chunk_size, sep, encodeur, file_type)
 
     global length_chunks_to_process
     length_chunks_to_process = len(chunks_to_process)
@@ -144,7 +166,7 @@ if __name__ == "__main__":  # Variables globales
     with Pool(processes=num_workers, initializer=initialize_client) as pool:
         results = pool.map(process_and_follow_chunk, chunks_to_process)
 
-    save_dataframes(results)
+    save_dataframes(results, file_type)
     traitement_time = time.time() - start
 
     print(f"Traitement et sauvegarde terminés en {round(traitement_time/60)} minutes")
