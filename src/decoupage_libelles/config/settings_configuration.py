0f90efac3a5d12b0d6d5d0ef924fea43130179b9
rayaberova <raya.berova@insee.fr>
2025-02-24 10:12:57 +0000

    changement des chemins de fichiers

--- src/decoupage_libelles/config/settings_configuration.py
+++ src/decoupage_libelles/config/settings_configuration.py
@@ -21,9 +21,10 @@ settings = Dynaconf(
     ],
 )
 
-settings.chemin_nlp_modele = "../data/fr_dep_news_trf-3.7.0/fr_dep_news_trf/fr_dep_news_trf-3.7.0/"
-settings.chemin_type_voie = "../data/type_voie.csv"
-settings.chemin_code2lib = "../data/code2lib.json"
+root = os.getcwd()
+settings.chemin_nlp_modele = root + "../data/fr_dep_news_trf-3.7.0/fr_dep_news_trf/fr_dep_news_trf-3.7.0/"
+settings.chemin_type_voie = root + "../data/type_voie.csv"
+settings.chemin_code2lib = root + "../data/code2lib.json"
 
 # `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
