6972cdfd27966cd6e674c5b8356c39b70f75312e
rayaberova <raya.berova@insee.fr>
2025-04-16 16:15:17 +0000

    pour creer les jeux de test, les mettre sur le s3

--- src/decoupage_libelles/scripts_parallelises/process_manual_labelisation.py
+++ src/decoupage_libelles/scripts_parallelises/process_manual_labelisation.py
@@ -1,59 +1,77 @@
 import pandas as pd
 import numpy as np
-
-# sortir les tables de DBEAVER
-# faire ca avec duckDB
-
-# SELECT h.nom_voie, h.type_voie_parse, h.libelle_voie_parse, h.complement_adresse  
-# FROM public.historique_chargements_ban h
-# WHERE h.id_chargement = 195
-#   AND h.complement_adresse IS NOT NULL
-#   AND h.complement_adresse != ' '
-# ORDER BY RANDOM()
-# LIMIT 3000;
-
-# SELECT h.nom_voie, h.type_voie_parse, h.libelle_voie_parse, h.complement_adresse  
-# FROM public.historique_chargements_ban h
-# WHERE h.id_chargement = 195
-# ORDER BY RANDOM()
-# LIMIT 5000;
+import duckdb
+import getpass
 
 
-# SELECT h.dvoilib, h.type_voie_parse, h.libelle_voie_parse, h.complement_adresse  
-# FROM public.historique_chargements_majic h
-# WHERE h.id_chargement = 7
-# ORDER BY RANDOM()
-# LIMIT 5000;
+# Activer et charger l'extension PostgreSQL
+conn = duckdb.connect()
+# Si postgres n'est pas installé, lancer la ligne suivante (sinon le télécharger manuellement sur internet)
+# conn.execute("INSTALL postgres;")
+conn.execute("LOAD postgres;")
 
+host = 'pdgaialg001'
+port = 1983
+database = 'gaia'
+user = (getpass.getpass("Username ?")).lower() # a changer pour que le code bloque sans ca
+password = getpass.getpass("Mot de passe ? ")  # a changer pour que le code bloque sans ca
 
-# SELECT h.denomination_de_voie_type, h.denomination_de_voie_libelle, h.type_voie_parse, h.libelle_voie_parse, h.complement_adresse  
-# FROM public.historique_chargements_rca h
-# WHERE h.id_chargement = 206
-# ORDER BY RANDOM()
-# LIMIT 5000;
+query_secret = f"""
+CREATE SECRET (
+    TYPE POSTGRES,
+    HOST '{host}',
+    PORT {port},
+    DATABASE {database},
+    USER '{user}',
+    PASSWORD '{password}'
+);
+"""
+conn.execute(query_secret)
 
-# source = "majic"
-# variables_type_nom_voie = ["dvoilib"]
+# prod
+dbname = "pi_pg_gaia_pd01"
+query_bdd = f"""
+ATTACH 'dbname={dbname}' AS pd_db (TYPE POSTGRES, READ_ONLY, SCHEMA 'public')
+;
+"""
+conn.execute(query_bdd)
+
+# choisir sa source
 
 # source = "ban"
-# variables_type_nom_voie = ["nom_voie"]
-
+# source = "majic"
 source = "rca"
-variables_type_nom_voie = ['denomination_de_voie_type', 'denomination_de_voie_libelle']
+
+dict_variables_type_nom_voie = {'ban': ["nom_voie"], 'majic': ["dvoilib"], 'rca': ['denomination_de_voie_type', 'denomination_de_voie_libelle']}
+variables_type_nom_voie = dict_variables_type_nom_voie[source]
+
+# extraire les données
+
+requests_to_extract_echant = {
+    'ban': "SELECT h.nom_voie, h.type_voie_parse, h.libelle_voie_parse, h.complement_adresse FROM pd_db.public.historique_chargements_ban h WHERE h.id_chargement = 195 ORDER BY RANDOM() LIMIT 5000;",
+    'majic': "SELECT h.dvoilib, h.type_voie_parse, h.libelle_voie_parse, h.complement_adresse FROM pd_db.public.historique_chargements_majic h WHERE h.id_chargement = 7 ORDER BY RANDOM() LIMIT 5000;",
+    'rca': "SELECT h.denomination_de_voie_type, h.denomination_de_voie_libelle, h.type_voie_parse, h.libelle_voie_parse, h.complement_adresse FROM pd_db.public.historique_chargements_rca h WHERE h.id_chargement = 206 ORDER BY RANDOM() LIMIT 5000;",
+    'compl_ban': "SELECT h.nom_voie, h.type_voie_parse, h.libelle_voie_parse, h.complement_adresse FROM pd_db.public.historique_chargements_ban h WHERE h.id_chargement = 195 AND h.complement_adresse IS NOT NULL AND h.complement_adresse != ' ' ORDER BY RANDOM() LIMIT 3000;"
+}
+
+df = conn.execute(requests_to_extract_echant[source]).fetchdf()
+
 
 # selectionner 3k lignes sans doublons
-
-df = pd.read_csv(f"C:/Users/FI7L7T/Documents/gaia/voies_echant_{source}.csv")
 
 df = df.drop_duplicates(subset=variables_type_nom_voie)
 
 df = df.iloc[:3000]
 
+# mettre sur le s3
 df.to_csv(f"C:/Users/FI7L7T/Documents/gaia/voies_echant_{source}.csv", index=False)
+
+# lancer le parsing
 
 # apres avoir parsé les données et retraité les colonnes manuellement
 # pour avoir cette structure id_type_voie_query	nom_voie_norm_query	complement_adresse_query	id_type_voie_match1	nom_voie_norm_match1	complement_adresse_match1	id_type_voie_match2	nom_voie_norm_match2	complement_adresse_match2
 
+# recupérer du parsing sur le s3
 df = pd.read_csv(f"C:/Users/FI7L7T/Documents/gaia/voies_echant_{source}_parsed.csv")
 
 # Découper en DataFrames de 1000 lignes
@@ -94,6 +112,7 @@ df_indecis = df[df['similarity'] == 'Undecided'][col_names_2]
 df_indecis = df_indecis.rename(columns=new_col_names_2)
 
 df_final = pd.concat([df_both, df_acc1, df_acc2, df_rej, df_indecis], ignore_index=True)
+df_final = df_final.replace(' ', '', regex=True)
 
 df_final.to_csv(f"C:/Users/FI7L7T/Documents/gaia/echant_voies/voies_echant_{source}_{i}_to_remake.csv", index=False)
 
@@ -102,3 +121,4 @@ df_final.to_csv(f"C:/Users/FI7L7T/Documents/gaia/echant_voies/voies_echant_{sour
 df_retravaille = pd.read_csv(f"C:/Users/FI7L7T/Documents/gaia/echant_voies/voies_echant_{source}_{i}_to_remake.csv")
 df_retravaille.drop(columns=['justification'], inplace=True)
 df_retravaille.to_csv(f"C:/Users/FI7L7T/Documents/gaia/echant_voies/voies_echant_{source}_{i}_done.csv", index=False)
+
