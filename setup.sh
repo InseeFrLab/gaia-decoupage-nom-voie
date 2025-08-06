dfdd505b5f30f9cc092887962579a9ff6f94090f
rayaberova <raya.berova@insee.fr>
2025-08-06 08:01:14 +0000

    montée de version minimale

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 pip install -r requirements.txt
+# pip install spacy-curated-transformers==2.0.0 --no-deps
 python -m unittest
 unzip data/fr_dep_news_trf-3.8.0-py3-none-any.zip -d data/fr_dep_news_trf-3.8.0/
 rm data/fr_dep_news_trf-3.8.0-py3-none-any.zip
